c44f921e665e1e68e9c207125110237d7456e10c
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2024-09-23 21:47:16 +0200

    dostoswanie widoku koszyka do malych wyswietlaczy

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const menuContent = document.querySelector('.nav-menu-content');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const userActions = document.querySelector('.user-actions');
 
     menuToggle.addEventListener('click', function() {
         menuContent.classList.toggle('open');
     });
+    burgerMenu.addEventListener('click', function() {
+        userActions.classList.toggle('open');
+});
 });
